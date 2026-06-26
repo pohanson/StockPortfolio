cbbb4a33d93e85c90a8427864d0885296f3ea03a
PohAnson <50758680+pohanson@users.noreply.github.com>
2026-06-26 08:21:36 +0800

    Fix errors in login

--- stock-portfolio-frontend/pages/api/login.ts
+++ stock-portfolio-frontend/pages/api/login.ts
@@ -3,14 +3,13 @@ import { NextApiResponse } from "next/types";
 export default async function loginRoute(req, res: NextApiResponse) {
   // get user from database then
   console.log(req.method, "/api/login");
-  await fetch(process.env.API_URL + "/user", {
+  await fetch(process.env.API_URL + "/login", {
     method: "POST",
     body: JSON.stringify(req.body),
     headers: {
       "Content-Type": "application/json",
     },
   }).then(async (r) => {
-    console.log(r.headers);
     if (r.status == 200) {
       res
         .setHeader("Set-Cookie", r.headers.get("set-cookie") || "")
